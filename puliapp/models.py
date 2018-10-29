636745d42225c924c9be77eba320aaddea871587
big data for SECOM <peter0123chang@gmail.com>
2018-10-29 16:55:35 +0800

    Update models.py

--- puliapp/models.py
+++ puliapp/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://tuismopqmafzhj:02dc15e1f6c6a03c8016276976f69fd1b234ab5a3c977b5d2ec3f57c23b23df6@ec2-54-83-49-109.compute-1.amazonaws.com:5432/deipbatmggnae8'
 class maplist(models.Model):	
 	mapName = models.CharField(max_length=60, null=False)	
 	mapDesc = models.TextField(null=False)	
