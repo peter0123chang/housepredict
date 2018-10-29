3114f9ac3335d8d88e93d46a3616d76cf4610636
big data for SECOM <peter0123chang@gmail.com>
2018-10-29 16:47:00 +0800

    Update wsgi.py

--- pulifood/wsgi.py
+++ pulifood/wsgi.py
@@ -21,7 +21,7 @@ application = get_wsgi_application()
 import os
 
 from django.core.wsgi import get_wsgi_application
-
+from dj_static import Cling
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pulifood.settings")
-
-application = get_wsgi_application()
+application = Cling(get_wsgi_application())
+#application = get_wsgi_application()
